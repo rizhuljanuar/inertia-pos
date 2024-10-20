cf757c610ff00d9c2d2139519b6f57cc7d01ce76
rizhul.januar <rayjam66@gmail.com>
2024-10-20 17:59:29 +0700

    feat: create auth, dashboard, permission

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+//route home
 Route::get('/', function () {
-    return view('welcome');
+    return \Inertia\Inertia::render('Auth/Login');
+})->middleware('guest');
+
+//prefix "apps"
+Route::prefix('apps')->group(function() {
+
+    //middleware "auth"
+    Route::group(['middleware' => ['auth']], function () {
+
+        //route dashboard
+        Route::get('dashboard', App\Http\Controllers\Apps\DashboardController::class)->name('apps.dashboard');
+        
+        //route permissions
+        Route::get('/permissions', \App\Http\Controllers\Apps\PermissionController::class)->name('apps.permissions.index')
+            ->middleware('permission:permissions.index');
+
+    });
 });
